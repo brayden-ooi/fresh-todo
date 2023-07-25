e4b2f15c0f0898fc24b22d145b6b9ab6b5d3a128
Brayden Ooi <brayden.ooi@hotmail.com>
2023-07-26 01:21:35 +0800

    update component to use async

--- routes/joke.tsx
+++ routes/joke.tsx
@@ -1,25 +1,45 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
+import { RouteContext } from "$fresh/server.ts";
 
-export const handler: Handlers<string | null> = {
-  async GET(_, ctx) {
-    const resp = await fetch(`http://localhost:8000/api/joke`);
-    if (resp.status === 404) {
-      return ctx.render(null);
-    }
-    const joke: string = await resp.text();
+// export const handler: Handlers<string | null> = {
+//   async GET(_, ctx) {
+//     const resp = await fetch(`http://localhost:8000/api/joke`);
+//     if (resp.status === 404) {
+//       return ctx.render(null);
+//     }
+//     const joke: string = await resp.text();
 
-    return ctx.render(joke);
-  },
-};
+//     return ctx.render(joke);
+//   },
+// };
 
-export default function Page({ data }: PageProps<string | null>) {
-  if (!data) {
+// export default function Page({ data }: PageProps<string | null>) {
+//   if (!data) {
+//     return <h1>Joke not found</h1>;
+//   }
+
+//   return (
+//     <div>
+//       <p>{data}</p>
+//     </div>
+//   );
+// }
+
+export default async function Page(req: Request, ctx: RouteContext) {
+  const resp = await fetch(`http://localhost:8000/api/joke`);
+
+  if (resp.status === 404) {
+    return <h1>Joke not found</h1>;
+  }
+
+  const joke: string = await resp.text();
+
+  if (!joke) {
     return <h1>Joke not found</h1>;
   }
 
   return (
     <div>
-      <p>{data}</p>
+      <p>{joke}</p>
     </div>
   );
 }
